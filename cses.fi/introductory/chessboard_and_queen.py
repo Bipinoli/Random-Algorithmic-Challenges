6d91212abe2283d1a901823af83fc14adc31bf1c
boli <bipinoli90@gmail.com>
2023-12-15 12:56:29 +0100

    solved another problem

--- cses.fi/introductory/chessboard_and_queen.py
+++ cses.fi/introductory/chessboard_and_queen.py
@@ -1,35 +1,59 @@
 n = 8
-free, reserved, queen = 0, 1, 8
+FREE, RESERVED, INVALID = 0, 1, 8
+
+
+
+def place_queen(x, row, board):
+    direction = [(-1,1), (0,1), (1,1)]
+    invalids = []
+    for d in direction:
+        cx, cy = x, row
+        while cx >= 0 and cy >= 0 and cx < n and cy < n:
+            if board[cy][cx] == FREE:
+                board[cy][cx] = INVALID
+                invalids.append((cy, cx))
+            cx += d[0]
+            cy += d[1]
+    return list(set(invalids))
+
+
+
+
+def solve(row, q, board):
+    '''
+        Putting queen row by row
+        row = current row
+        q = queens left
+    '''
+    if row == n:
+        if q > 0:
+            return 0
+        else: 
+            return 1
+    
+    ans = 0
+    for x in range(n):
+        if board[row][x] == FREE:
+            invalids = place_queen(x, row, board)
+            ans += solve(row+1, q-1, board)
+            for pos in invalids:
+                r, c = pos
+                board[r][c] = FREE
+
+    return ans
+
+
+    
+    
 board = []
 for i in range(n):
     board.append([])
     for c in input():
         if c == ".":
-            board[i].append(free)
+            board[i].append(FREE)
         else:
-            board[i].append(reserved)
-
-
-def is_valid(x, y, state):
-    if state[y][x] != free:
-        return False
-    direction = [(0,1), (0,-1), (1,0), (0,1), (-1,-1), (1,1), (-1,1),(1,-1)]
-    for d in direction:
-        cx, cy = x, y
-        while cx >= 0 and cy >= 0 and cx < n and cy < n:
-            if state[cy][cx] == queen:
-                return False
-            cx += d[0]
-            cy += d[1]
-    return True
-
-
-def display():
-    for i in range(n):
-        for j in range(n):
-            print(board[i][j], end="")
-        print()
+            board[i].append(RESERVED)
 
 
 
-
+print(solve(0, n, board))
